ec159e6d2a48a2d1b85ed6bd6c543cd70dff463f
Kyle <kylcrow@gmail.com>
2010-10-02 19:57:24 -0400

    test commit

---
+++ Classes/DailyAppAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DailyAppAppDelegate.h
+//  DailyApp
+//
+//  Created by Kyle Crow on 10/2/10.
+//  Copyright 2010 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailyAppAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
---
+++ Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  DailyApp
+//
+//  Created by Kyle Crow on 10/2/10.
+//  Copyright 2010 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
